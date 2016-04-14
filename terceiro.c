28a3d5b5194e291611b417aa52755c5bf7b08d58
victor cunha <vcunha1503@gmail.com>
2016-04-14 13:30:25 +0000

    variaveis

---
+++ terceiro.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    int soma;
+    
+    num1 = 9;
+    num2 = 10;
+    
+    soma= num1 + num2;
+    printf("\nsoma = %d\n\n", soma);
+    
+    return 0;
+}
