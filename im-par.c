b5ac44405cadd7dec8ddac04c01a74938bc55615
victor cunha <vcunha1503@gmail.com>
2016-04-18 13:25:18 +0000

    par ou impar

---
+++ im-par.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+ 
+int main() 
+{
+     int n1,n2,resto;    
+      
+     printf ("\nDigite um numero:");
+     scanf ("%d", &n1); 
+      
+     printf ("\nDigite um numero:");
+     n2 = 2 ;
+     
+    if(n1%n2 == 0){
+    printf ("par");
+    }
+     else{
+     
+    printf ("impar");
+     } 
+     }
