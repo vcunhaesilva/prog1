9c75e4b93ab505342ca118c8d8c202da4207fafa
victor cunha <vcunha1503@gmail.com>
2016-04-18 12:09:09 +0000

    funcionando

--- terceiro.c
+++ terceiro.c
@@ -3,13 +3,17 @@ int main()
 {
     int num1, num2;
     int soma;
+    
+    //entre com o primeiro numero
     printf("primeiro numero: "); 
     scanf("%d",&num1);
+    //entre com o segundo numero
     printf("segundo numero: ");
     scanf("%d",&num2);
         
     soma = num1 + num2;
     
+    //resultado da soma dos numeros inseridos
     printf("\n%d\n\n",soma);
   
    
